39d9ed88aa473d5e403d2c9fc9ec4931a79457de
Amelbo3 <amelbo3@gmail.com>
2025-04-18 18:05:13 -0400

    ajout du module banques.py
    Signed-off-by: Amelbo3 <amelbo3@gmail.com>

--- app/server.py
+++ app/server.py
@@ -11,6 +11,20 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
-@application.get("/home")
-def read_home():
-    return {"home": "Address de ma maison."}
+@application.get("/email/{email}")
+def read_email(email):
+    if email == 'amelbo3@gmail.com':
+    
+        nom = "Amadou"
+        prenom = "Elhadj"
+        dob = "04/27/1974"
+        city = "Charlotte"
+        phone = "980-333-5085"
+
+        mes_donnees = {"nom": nom, "prenom": prenom, "dob": dob, "city": city, "phone": phone}
+    else:
+
+        mes_donnees = {"nom": "", "prenom": "", "dob": "", "city": "", "phone": ""}
+    
+
+    return mes_donnees
